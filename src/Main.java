073ec9aff0d37596b81a4d378bfd4d9ac3a82463
felipejesus-front <felipe.campos@al.resilia.com.br>
2026-01-04 14:48:06 -0300

    usando a maioria das mudanças.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import br.com.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.screenmatch.calculos.FiltroRecomendacao;
 import br.com.screenmatch.model.Filme;
+import br.com.screenmatch.model.Serie;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        CalculadoraDeTempo calcula = new CalculadoraDeTempo();
         Filme meuFilme = new Filme();
+        Serie minhaSerie = new Serie();
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+
         meuFilme.setNome("Alto da compadecida");
         meuFilme.setDuracaoEmMinutos(30);
         meuFilme.setAnoDeLancamento(2002);
@@ -14,7 +20,22 @@ public class Main {
         meuFilme.avalia(9);
         meuFilme.avalia(10);
         meuFilme.avalia(7);
-
         meuFilme.exibirFichaTecnica();
+        filtro.filtraPorClassificacao(meuFilme);
+
+
+        minhaSerie.setNome("Breaking Bad");
+        minhaSerie.setTemporadas(5);
+        minhaSerie.setEpisodiosPorTemporada(13);
+        minhaSerie.setMinutosPorEpisodio(45);
+        minhaSerie.setAnoDeLancamento(2002);
+        minhaSerie.avalia(10);
+        minhaSerie.exibirFichaTecnica();
+        filtro.filtraPorClassificacao(minhaSerie);
+
+        calcula.somaTempo(meuFilme);
+        calcula.somaTempo(minhaSerie);
+
+        System.out.println("Tempo total da sua lista: " + calcula.getTempoTotal() + " minutos");
     }
 }
